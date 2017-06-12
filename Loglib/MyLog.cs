9a3c2db68a452ab35ea2a1e7cc8e3b723eb391b5
luoyq <waiterlyq@163.com>
2017-06-12 22:36:01 +0800

    初始化

---
+++ Loglib/MyLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using log4net;
+
+namespace Loglib
+{
+    public enum logtype { Info, Debug, Warn, Fatal, Error }
+    public class MyLog
+    {
+        public static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// 分类型记录日志
+        /// </summary>
+        /// <param name="Type">类型</param>
+        /// <param name="text"></param>
+        public static void writeLog(string text, logtype Type = logtype.Error,Exception e=null)
+        {
+            switch (Type)
+            {
+                case logtype.Info:
+                    log.Info(text);
+                    break;
+                case logtype.Debug:
+                    log.Debug(text);
+                    break;
+                case logtype.Warn:
+                    log.Warn(text);
+                    break;
+                case logtype.Fatal:
+                    log.Fatal(text, e);
+                    break;
+                default:
+                    log.Error(text, e);
+                    break;
+            }
+        }
+    }
+}
