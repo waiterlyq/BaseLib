ee350f781508e410c67d2e2ce046e165de9268eb
luoyq <waiterlyq@163.com>
2017-07-25 11:19:25 +0800

    提交

--- Excellib/ExcelHelper.cs
+++ Excellib/ExcelHelper.cs
@@ -2,49 +2,196 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Data;
-using System.IO;
 using NPOI.SS.UserModel;
-using NPOI.HSSF.UserModel;
 using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using System.IO;
+using System.Data;
+using Loglib;
 
-namespace Excellib
+
+namespace NetUtilityLib
 {
-    public class ExcelHelper
+    public class ExcelHelper : IDisposable
     {
-        public DataTable GetDtByExcel(string filePath)
+        private string fileName = null; //文件名
+        private IWorkbook workbook = null;
+        private FileStream fs = null;
+        private bool disposed;
+
+        public ExcelHelper(string fileName)
         {
-            DataTable dt = new DataTable();
-            IWorkbook wb = null;
-            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            if (filePath.IndexOf(".xlsx") > 0) // 2007版本  
+            this.fileName = fileName;
+            disposed = false;
+        }
+
+        /// <summary>
+        /// 将DataTable数据导入到excel中
+        /// </summary>
+        /// <param name="data">要导入的数据</param>
+        /// <param name="isColumnWritten">DataTable的列名是否要导入</param>
+        /// <param name="sheetName">要导入的excel的sheet的名称</param>
+        /// <returns>导入数据行数(包含列名那一行)</returns>
+        public int DataTableToExcel(DataTable data, string sheetName, bool isColumnWritten)
+        {
+            int i = 0;
+            int j = 0;
+            int count = 0;
+            ISheet sheet = null;
+
+            fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            if (fileName.IndexOf(".xlsx") > 0) // 2007版本
+                workbook = new XSSFWorkbook();
+            else if (fileName.IndexOf(".xls") > 0) // 2003版本
+                workbook = new HSSFWorkbook();
+
+            try
             {
-                wb = new XSSFWorkbook(fileStream);  //xlsx数据读入workbook  
-            }
-            else if (filePath.IndexOf(".xls") > 0) // 2003版本  
-            {
-                wb = new HSSFWorkbook(fileStream);  //xls数据读入workbook  
-            }
-            else
-            {
-                return dt;
-            }
-            ISheet ish = wb.GetSheetAt(0);
-            IRow header = ish.GetRow(ish.FirstRowNum);
-            for(int i = 0;i<header.LastCellNum;i++)
-            {
-               // dt.Columns.Add(header.GetCell(i).StringCellValue, header.GetCell(i).CellType);
-            }
-            for(int i = ish.FirstRowNum+1; i <ish.LastRowNum;i++)
-            {
-                DataRow dr = dt.NewRow();
-                for(int j=0;j<header.LastCellNum;j++)
+                if (workbook != null)
                 {
-                    dr[j] = ;
+                    sheet = workbook.CreateSheet(sheetName);
                 }
-            } 
-            return dt;
+                else
+                {
+                    return -1;
+                }
+
+                if (isColumnWritten == true) //写入DataTable的列名
+                {
+                    IRow row = sheet.CreateRow(0);
+                    for (j = 0; j < data.Columns.Count; ++j)
+                    {
+                        row.CreateCell(j).SetCellValue(data.Columns[j].ColumnName);
+                    }
+                    count = 1;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+                for (i = 0; i < data.Rows.Count; ++i)
+                {
+                    IRow row = sheet.CreateRow(count);
+                    for (j = 0; j < data.Columns.Count; ++j)
+                    {
+                        row.CreateCell(j).SetCellValue(data.Rows[i][j].ToString());
+                    }
+                    ++count;
+                }
+                workbook.Write(fs); //写入到excel
+                return count;
+            }
+            catch (Exception e)
+            {
+                MyLog.writeLog("ERROR", e);
+                return -1;
+            }
+        }
+
+        /// <summary>
+        /// 将excel中的数据导入到DataTable中
+        /// </summary>
+        /// <param name="sheetName">excel工作薄sheet的名称</param>
+        /// <param name="isFirstRowColumn">第一行是否是DataTable的列名</param>
+        /// <returns>返回的DataTable</returns>
+        public DataTable ExcelToDataTable(string sheetName, bool isFirstRowColumn)
+        {
+            ISheet sheet = null;
+            DataTable data = new DataTable();
+            int startRow = 0;
+            try
+            {
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                if (fileName.IndexOf(".xlsx") > 0) // 2007版本
+                    workbook = new XSSFWorkbook(fs);
+                else if (fileName.IndexOf(".xls") > 0) // 2003版本
+                    workbook = new HSSFWorkbook(fs);
+
+                if (sheetName != null)
+                {
+                    sheet = workbook.GetSheet(sheetName);
+                    if (sheet == null) //如果没有找到指定的sheetName对应的sheet，则尝试获取第一个sheet
+                    {
+                        sheet = workbook.GetSheetAt(0);
+                    }
+                }
+                else
+                {
+                    sheet = workbook.GetSheetAt(0);
+                }
+                if (sheet != null)
+                {
+                    IRow firstRow = sheet.GetRow(0);
+                    int cellCount = firstRow.LastCellNum; //一行最后一个cell的编号 即总的列数
+
+                    if (isFirstRowColumn)
+                    {
+                        for (int i = firstRow.FirstCellNum; i < cellCount; ++i)
+                        {
+                            ICell cell = firstRow.GetCell(i);
+                            if (cell != null)
+                            {
+                                string cellValue = cell.StringCellValue;
+                                if (cellValue != null)
+                                {
+                                    DataColumn column = new DataColumn(cellValue);
+                                    data.Columns.Add(column);
+                                }
+                            }
+                        }
+                        startRow = sheet.FirstRowNum + 1;
+                    }
+                    else
+                    {
+                        startRow = sheet.FirstRowNum;
+                    }
+
+                    //最后一列的标号
+                    int rowCount = sheet.LastRowNum;
+                    for (int i = startRow; i <= rowCount; ++i)
+                    {
+                        IRow row = sheet.GetRow(i);
+                        if (row == null) continue; //没有数据的行默认是null　　　　　　　
+
+                        DataRow dataRow = data.NewRow();
+                        for (int j = row.FirstCellNum; j < cellCount; ++j)
+                        {
+                            if (row.GetCell(j) != null) //同理，没有数据的单元格都默认是null
+                                dataRow[j] = row.GetCell(j).ToString();
+                        }
+                        data.Rows.Add(dataRow);
+                    }
+                }
+
+                return data;
+            }
+            catch (Exception e)
+            {
+                MyLog.writeLog("ERROR", e);
+                return null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    if (fs != null)
+                        fs.Close();
+                }
+
+                fs = null;
+                disposed = true;
+            }
         }
     }
 }
---
+++ Filelib/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Filelib
+{
+    public class FileHelper
+    {
+        public static string GetFilePath(string rootPath,string fileName,params string[] folders)
+        {
+            string folderpath = rootPath;
+            foreach(string fo in folders)
+            {
+                folderpath += "\\"+fo+"\\";
+            }
+            if(string.IsNullOrEmpty(folderpath))
+            {
+                return "";
+            }
+            if(!Directory.Exists(folderpath))
+            {
+                Directory.CreateDirectory(folderpath);
+            }
+            string FilePath = folderpath+"\\"+ fileName;
+            return FilePath;
+        }
+    }
+}
