f2d882517bedf63b72dbf12f3f6f6a75b411039b
luoyq <waiterlyq@163.com>
2017-08-20 22:30:20 +0800

    提交

---
+++ Wxlib/Wxlib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wxlib
+{
+    /// <summary>
+    /// 微信access_token
+    /// </summary>
+    public class WxAt
+    {
+        
+    }
+}
