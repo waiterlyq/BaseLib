63a37b79e90702d5a8e1a8e33934d88ae04d09a0
luoyq <waiterlyq@163.com>
2017-08-22 18:50:48 +0800

    提交

--- Wxlib/Wxlib.cs
+++ Wxlib/Wxlib.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Configuration;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Loglib;
 
 namespace Wxlib
 {
@@ -11,6 +17,33 @@ namespace Wxlib
     /// </summary>
     public class WxAt
     {
-        
+        public string access_token;
+        public int expires_in;
+        public WxAt()
+        {
+            getAccess_Token();
+        }
+
+        public void getAccess_Token()
+        {
+            string appid = ConfigurationManager.AppSettings["appid"];
+            string appsecret = ConfigurationManager.AppSettings["appsecret"];
+            string tokenUrl = @"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appid + "&secret=" + appsecret;
+            WebClient wc = new WebClient();
+            string strReturn = wc.DownloadString(tokenUrl);
+            JObject array;
+            
+            try
+            {
+                array = (JObject)JsonConvert.DeserializeObject(strReturn);
+                access_token = array["access_token"].ToString().Trim('"');
+                expires_in = int.Parse(array["expires_in"].ToString());
+            }
+            catch(Exception e) {
+                MyLog.writeLog(strReturn, e);
+                throw (e);
+            }
+        }
     }
+    
 }
